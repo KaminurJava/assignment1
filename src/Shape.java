e2e56091d45cb5e4f12454e0b461127488f07d53
KaminurJava <153558671+KaminurJava@users.noreply.github.com>
2024-01-12 19:43:31 +0600

    Shape.java

--- src/Shape.java
+++ src/Shape.java
@@ -1,35 +1,55 @@
 import java.util.ArrayList;
 public class Shape {
     private ArrayList<Point> points;
-    public Shape() {        
+
+    public Shape() {
         this.points = new ArrayList<>();
     }
-    public void addPoint(Point point) {        
+
+    public void addPoint(Point point) {
+
         points.add(point);
     }
-    public double calculatePerimeter() {        
+
+    public double calculatePerimeter() {
         double perimeter = 0;
         int size = points.size();
-        for (int i = 0; i < size; i++) {            
+        for (int i = 0; i < size; i++) {
             Point currentPoint = points.get(i);
-            Point nextPoint = points.get((i + 1) % size);            
+            Point nextPoint = points.get((i + 1) % size);
             perimeter += currentPoint.distance(nextPoint);
         }
-        return perimeter;    
+        return perimeter;
+
     }
+
     public double getAverageSide() {
-        double totalDistance = calculatePerimeter();        
+        double totalDistance = calculatePerimeter();
         int size = points.size();
-        return totalDistance / size;    }
+        return totalDistance / size;
+    }
+
     public double getLongestSide() {
         double longestSide = 0;
-        for (int i = 0; i < points.size(); i++) {            
+        for (int i = 0; i < points.size(); i++) {
             Point currentPoint = points.get(i);
-            Point nextPoint = points.get((i + 1) % points.size());            
+            Point nextPoint = points.get((i + 1) % points.size());
             double distance = currentPoint.distance(nextPoint);
-            if (distance > longestSide) {                
+            if (distance > longestSide) {
                 longestSide = distance;
-            }        
+            }
         }
         return longestSide;
-    }}
+    }
+
+    public int getNumberOfPoints() {
+        return points.size();
+    }
+
+    public Point getPoint(int i) {
+        if (i >= 0 && i < points.size()) {
+            return points.get(i);
+        }
+        return null;
+    }
+}
